696c70f220b81ddec2ca5e2abc370af88bd3513a
IgorAPSantos <igorppandre@hotmail.com>
2023-11-03 13:15:26 -0300

    fix: adicionando o not null no dump

--- dump.sql
+++ dump.sql
@@ -2,14 +2,14 @@ create database pdv;
 
 create table usuarios(
   id serial primary key,
-  nome text,
-  email text unique,
-  senha text
+  nome text not null,
+  email text unique not null,
+  senha text not null
 );
 
 create table categorias(
   id serial primary key,
-  descricao text
+  descricao text not null
 );
 
 insert into categorias (descricao) 
