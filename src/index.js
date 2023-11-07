ac658d05384c3ed9ac43e7bd397c848bee297232
IgorAPSantos <igorppandre@hotmail.com>
2023-11-07 16:33:04 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 
-const app  = express();
+const app = express();
 const rotas = require('./rotas')
 
+app.use(cors())
 app.use(express.json());
 app.use(rotas)
 
