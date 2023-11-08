1a66fadc76ce2098a13169326a4d82ac283ad565
IgorAPSantos <igorppandre@hotmail.com>
2023-11-07 23:21:30 -0300

    refactor: refatorando a rota editarUsuario

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -38,41 +38,30 @@ const detalharPerfilUsuarioLogado = async (req, res) => {
 
 const editarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body
+    const { id } = req.usuario
 
-    try {        
-        if (nome || email || senha) {
-            const { id } = req.usuario
+    try {
+        const emailJaExiste = await knex('usuarios').where({ email }).andWhere('id', '!=', id).first()
 
-            const usuario = await knex('usuarios').where({ id }).first()
-
-            if (email) {
-                // verificar se email ja existe para outro usuario
-                const resp = await knex('usuarios').select('*').where({ email }).andWhere('id', '!=', id).returning('*')
-
-                if (resp.length > 0) {
-                    return res.status(400).json({ mensagem: "Email ja existe no banco de dados" })
-                }
-            }
-
-            const senhaCriptografada = await bcrypt.hash(senha, 10)
-
-            const usuarioEditado = {
-                nome: nome ? nome : usuario.nome,
-                email: email ? email : usuario.email,
-                senha: senha ? senhaCriptografada : usuario.senha
-            }
-
-            await knex('usuarios').update({ nome: usuarioEditado.nome, email: usuarioEditado.email, senha: usuarioEditado.senha })
-
-            return res.status(200).json({ mensagem: "Usuario editado com sucesso" })
-
-        } else {
-            return res.status(400).json({ mensagem: "Para editar o seu perfil de usuario, por favor informe nome, email e senha" })
+        if (emailJaExiste) {
+            return res.status(400).json({ mensagem: "Este email ja existe no banco de dados" })
         }
 
-    } catch (error) {        
+        const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+        const usuarioEditado = await knex('usuarios').update({ nome, email, senha: senhaCriptografada }).returning(['id', 'nome', 'email'])
+
+        if (!usuarioEditado[0]) {
+            return res.status(400).json({ mensagem: "Não foi possível atualizar o usuário" })
+        }
+
+        return res.status(200).json({ atuliazadoComSucesso: usuarioEditado[0] })
+
+    }
+
+    catch (error) {
         console.log(error)
-        return res.status(200).json({ mensagem: "Erro interno do servidor" })
+        return res.status(500).json({ mensagem: "Erro interno do servidor" })
     }
 }
 
