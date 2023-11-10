654a75aa219a6f42deb59691741fbb79a1c8c75c
IgorAPSantos <igorppandre@hotmail.com>
2023-11-10 10:53:18 -0300

    atualizando dumpSql

--- dump.sql
+++ dump.sql
@@ -1,7 +1,10 @@
 create database pdv;
 
+
 drop table if exists usuarios;
 drop table if exists categorias;
+drop table if exists produtos;
+drop table if exists clientes;
 
 create table usuarios(
   id serial primary key,
@@ -19,3 +22,23 @@ insert into categorias (descricao)
 values('Informática'), ('Celulares'), ('Beleza e Perfumaria'), 
 ('Mercado'), ('Livros e Papelaria'), ('Brinquedos'), ('Moda'),
 ('Bebê'), ('Games');
+
+
+create table produtos(
+  id serial primary key,
+  descricao text not null,
+  quantidade_estoque int not null,
+  valor int not null,
+  categoria_id int not null references categorias(id));
+  
+create table clientes(
+  id serial primary key,
+  nome text not null,
+  emai text not null unique,
+  cpf text not null unique,
+  cep text default null,
+  rua text default null,
+  numero text default null,
+  bairro text default null,
+  cidade text default null,
+  estado text default null);
