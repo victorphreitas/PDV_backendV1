e8fd2738444a9cd76aae4a913cafdf5aca98247a
IgorAPSantos <igorppandre@hotmail.com>
2023-11-14 21:17:27 -0300

    criada a rota para cadastro do endereco do cliente

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -1,31 +1,25 @@
 const knex = require('../conexoes/bancodedados')
 
 const cadastrarCliente = async (req, res) => {
-  const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body
+  const { nome, email, cpf } = req.body
 
   try {
     const emailJaExiste = await knex('clientes').where({ email }).first()
 
     if (emailJaExiste) {
-      return res.status(400).json({ mensagem: "Email já cadastrado." })
+      return res.status(400).json({ mensagem: "Este email já está cadastrado." })
     }
 
     const cpfJaExiste = await knex('clientes').where({ cpf }).first()
 
     if (cpfJaExiste) {
-      return res.status(400).json({ mensagem: "CPF já cadastrado." })
+      return res.status(400).json({ mensagem: "Este CPF já está cadastrado." })
     }
 
     const novoCliente = {
       nome,
       email,
-      cpf,
-      cep,
-      rua, 
-      numero,
-      bairro,
-      cidade,
-      estado
+      cpf
     }
 
     await knex('clientes').insert(novoCliente)
@@ -38,40 +32,34 @@ const cadastrarCliente = async (req, res) => {
 };
 
 const editarCliente = async (req, res) => {
-  const {nome, email, cpf, cep, rua, numero, bairro, cidade, estado} = req.body;
-  const {id} = req.params;
+  const { nome, email, cpf } = req.body;
+  const { id } = req.params;
 
   try {
     const cliente = await knex('clientes').where({ id }).first()
 
-    if(!cliente){
+    if (!cliente) {
       return res.status(404).json({ mensagem: 'Cliente não encontrado.' })
     }
 
-    const cpfJaExiste = await knex('clientes').where({cpf}).whereNot('id',id).first()
-    const emailJaExiste = await knex('clientes').where({email}).whereNot('id',id).first()
+    const cpfJaExiste = await knex('clientes').where({ cpf }).whereNot('id', id).first()
+    const emailJaExiste = await knex('clientes').where({ email }).whereNot('id', id).first()
 
-    if(cpfJaExiste){
-      return res.status(400).json({ mensagem: 'CPF já está cadastrado.' })
+    if (cpfJaExiste) {
+      return res.status(400).json({ mensagem: 'Este CPF já está cadastrado.' })
     }
 
-    if(emailJaExiste){
-      return res.status(400).json({ mensagem: 'Email já está cadastrado.' })
+    if (emailJaExiste) {
+      return res.status(400).json({ mensagem: 'Este email já está cadastrado.' })
     }
 
     const clienteAtualizado = await knex('clientes').update({
       nome,
       email,
-      cpf,
-      cep,
-      rua,
-      numero,
-      bairro,
-      cidade,
-      estado
-    }).where({id}).returning('*')
+      cpf
+    }).where({ id }).returning('*')
 
-    return res.json({clienteAtualizado: clienteAtualizado[0]})
+    return res.json({ clienteAtualizado: clienteAtualizado[0] })
 
   } catch (error) {
     return res.status(500).json({ mensagem: "Erro interno do servidor." })
@@ -79,11 +67,11 @@ const editarCliente = async (req, res) => {
 };
 
 const listarClientes = async (req, res) => {
-  
+
   try {
     const clientes = await knex('clientes')
-    
-    if(!clientes){
+
+    if (!clientes) {
       return res.status(404).json({ mensagem: 'Cliente(s) nao encontrado(s).' })
     }
 
@@ -95,25 +83,55 @@ const listarClientes = async (req, res) => {
 };
 
 const detalharCliente = async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
 
   try {
     const cliente = await knex('clientes').where({ id }).first()
 
-    if(!cliente){
-      return res.status(404).json({mensagem: 'Cliente não encontrado.'})
+    if (!cliente) {
+      return res.status(404).json({ mensagem: 'Cliente não encontrado.' })
     }
 
     return res.json(cliente)
-    
+
   } catch (error) {
     return res.status(500).json({ mensagem: "Erro interno do servidor." })
   }
 };
 
+const editarEnderecoCliente = async (req, res) => {
+  const { cep, rua, numero, bairro, cidade, estado } = req.body
+  const { id } = req.params
+
+  if (!cep && !rua && !numero && !bairro && !cidade && !estado) {
+    return res.status(400).json({ mensagem: 'É necessário preencher pelo menos um dos campos de endereço.' })
+  }
+
+  try {
+    const cliente = await knex('clientes').where({ id }).first()
+    if (!cliente) {
+      return res.status(404).json({ mensagem: 'Cliente não encontrado.' })
+    }
+    const novoEndereco = {
+      cep: cep ?? cliente.cep,
+      rua: rua ?? cliente.rua,
+      numero: numero ?? cliente.numero,
+      bairro: bairro ?? cliente.bairro,
+      cidade: cidade ?? cliente.cidade,
+      estado: estado ?? cliente.estado,
+    }
+
+    const clienteAtualizado = await knex('clientes').where({ id }).update(novoEndereco).returning('*')
+
+    return res.json(clienteAtualizado)
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor." })
+  }
+}
 module.exports = {
   cadastrarCliente,
   editarCliente,
   listarClientes,
-  detalharCliente
+  detalharCliente,
+  editarEnderecoCliente
 }
---
+++ src/validacoes/enderecoClienteSchema.js
@@ -0,0 +1,38 @@
+const joi = require("joi")
+
+
+const enderecoClienteSchema = joi.object({
+    cep: joi.string().pattern(new RegExp('^[0-9]{8}$')).messages({
+        'string.pattern.base': 'Informe um cep válido.',
+        'string.base': 'Informe um cep válido.',
+        'string.empty': 'Informe um cep válido.'
+    }),
+    rua: joi.string().max(140).messages({
+        'string.base': 'Informe uma rua válida.',
+        'string.empty': 'Informe uma rua válida.',
+        'string.max': 'Informe uma rua válida.'
+    }),
+    numero: joi.string().max(10).messages({
+        'string.base': 'Informe um numero válido.',
+        'string.empty': 'Informe um numero válido.',
+        'string.max': 'Informe um numero válido.'
+
+    }),
+    bairro: joi.string().max(140).messages({
+        'string.base': 'Informe um bairro válido.',
+        'string.empty': 'Informe um bairro válido.',
+        'string.max': 'Informe um bairro válido.'
+    }),
+    cidade: joi.string().max(140).messages({
+        'string.base': 'Informe uma cidade válida.',
+        'string.empty': 'Informe uma cidade válida.',
+        'string.max': 'Informe uma cidade válida.'
+    }),
+    estado: joi.string().max(2).messages({
+        'string.base': 'Informe um estado válido.',
+        'string.empty': 'Informe um estado válido.',
+        'string.max': 'Informe um estado válido.'
+    })
+})
+
+module.exports = enderecoClienteSchema
