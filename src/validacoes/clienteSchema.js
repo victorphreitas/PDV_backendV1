2837f74281458e7d219fd5ff212e0dd38e15dce5
IgorAPSantos <igorppandre@hotmail.com>
2023-11-23 22:16:41 -0300

    corrigindo rota de cadastro/atualizacao cliente

--- src/validacoes/clienteSchema.js
+++ src/validacoes/clienteSchema.js
@@ -23,26 +23,38 @@ const clienteSchema = joi.object({
         'string.base': 'Informe um cpf válido.'
 
     }),
-    cep: joi.string().allow('').pattern(/^[0-9]{8}$/).messages({
-        'string.pattern.base': 'Informe um cpf válido.',
-        'string.base': 'Informe um cpf válido.'
+    cep: joi.string().pattern(new RegExp('^[0-9]{8}$')).messages({
+        'string.pattern.base': 'Informe um cep válido.',
+        'string.base': 'Informe um cep válido.',
+        'string.empty': 'Informe um cep válido.'
+    }),
+    rua: joi.string().trim().max(140).messages({
+        'string.base': 'Informe uma rua válida.',
+        'string.empty': 'Informe uma rua válida.',
+        'string.max': 'Informe uma rua válida.'
+    }),
+    numero: joi.string().max(5).pattern(new RegExp(/^\d+[a-zA-Z]*$/)).messages({
+        'string.base': 'Informe um numero válido.',
+        'string.empty': 'Informe um numero válido.',
+        'string.max': 'Informe um numero válido.',
+        "string.pattern.base": 'Informe um numero válido.'
 
     }),
-    rua: joi.string().allow('').messages({
-        'string.base': 'Informe uma rua válida.'
+    bairro: joi.string().trim().max(140).messages({
+        'string.base': 'Informe um bairro válido.',
+        'string.empty': 'Informe um bairro válido.',
+        'string.max': 'Informe um bairro válido.'
     }),
-    numero: joi.string().allow('').max(10).messages({
-        'string.base': 'Informe uma rua válida.'
+    cidade: joi.string().trim().max(140).messages({
+        'string.base': 'Informe uma cidade válida.',
+        'string.empty': 'Informe uma cidade válida.',
+        'string.max': 'Informe uma cidade válida.'
     }),
-    bairro: joi.string().allow('').messages({
-        'string.base': 'Informe um bairro válido.'
-    }),
-    cidade: joi.string().allow('').messages({
-        'string.base': 'Informe uma cidade válida.'
-    }),
-    estado: joi.string().allow('').messages({
-        'string.base': 'Informe um estado válido.'
-    }),
+    estado: joi.string().trim().max(2).messages({
+        'string.base': 'Informe um estado válido.',
+        'string.empty': 'Informe um estado válido.',
+        'string.max': 'Informe um estado válido.'
+    })
 
 });
 
