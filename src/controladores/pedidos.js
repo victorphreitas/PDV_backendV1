ab69e428b5e9c6afdcb5989ceb76c3576be86589
IgorAPSantos <igorppandre@hotmail.com>
2023-11-23 21:00:17 -0300

    aprimorado a rota de cadastro de pedidos

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -18,46 +18,46 @@ const cadastrarPedido = async (req, res) => {
       let produto = pedido_produtos[i]
 
       const produtoExiste = await knex('produtos').where({ id: produto.produto_id }).first()
-  
+
       if (!produtoExiste) {
         return res.status(404).json({ mensagem: `O produto de id ${produto.produto_id} nao existe` })
       }
-      
-      if(produtoExiste.quantidade_estoque < produto.quantidade_produto) {
-        return res.status(400).json({ mensagem: `So existem ${produtoExiste.quantidade_estoque} produtos do produto de id ${produtoExiste.id}` })
+
+      if (produtoExiste.quantidade_estoque < produto.quantidade_produto) {
+        return res.status(400).json({ mensagem: `O pedido não foi realizado pois há apenas ${produtoExiste.quantidade_estoque} produto(s) no estoque do produto de id ${produtoExiste.id}.` })
       }
 
       valorTotal += produtoExiste.valor
-      
+      const { quantidade_estoque } = produtoExiste
+      const { quantidade_produto } = produto
+      produtoExiste.quantidade_estoque = quantidade_estoque - quantidade_produto
+      await knex('produtos').update({ quantidade_estoque: produtoExiste.quantidade_estoque }).where({ id: produto.produto_id })
       produtos.push(produtoExiste)
 
     }
-    
-    // cadastrar pedido no banco de dados 
+
     const pedido = await knex('pedidos').insert({ cliente_id, observacao, valor_total: valorTotal }).returning('*')
 
-    for (let i = 0; i < produtos.length; i++) {
-      const produto = produtos[i]
-      // cadastrar pedido_produtos no banco de dados 
-      await knex('pedido_produtos').insert({ pedido_id: pedido[0].id, produto_id: produto.id, quantidade_produto: produto.quantidade_estoque, valor_produto: produto.valor})
+    for (produto of produtos) {
+      await knex('pedido_produtos').insert({ pedido_id: pedido[0].id, produto_id: produto.id, quantidade_produto: produto.quantidade_estoque, valor_produto: produto.valor })
     }
-    // enviar email para o cliente 
+
     const email = {
       assunto: 'Pedido Cadastrado',
       corpoEmail: 'Seu pedido foi cadastrado com sucesso!'
     }
 
     emailSender(process.env.MAIL_SENDER, clienteExiste.email, email.assunto, email.corpoEmail)
-    .catch(error => console.log(error))
-    
+      .catch(error => console.log(error))
+
     return res.status(200).json({ mensagem: "Pedido cadastrado com sucesso" })
- 
+
   } catch (error) {
-    console.log(error) 
+    console.log(error)
     return res.status(500).json({ mensagem: "Erro interno do servidor" })
   }
 }
- 
+
 module.exports = {
   cadastrarPedido
 }
